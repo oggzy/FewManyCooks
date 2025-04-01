0eb473297c3b6ddb8b69d22b6351966c98fb45bd
oscar ogilvie <osc.ogilvie@gmail.com>
2025-04-01 17:50:43 +0100

    renamed and refactored data functions file, moved test data to test

--- test/test_calc.py
+++ test/test_calc.py
@@ -1,11 +1,12 @@
-from calc import get_recipe, strip_ingredients
+from food_data import get_recipe, strip_ingredients
 
 def test_get_recipe_returns_dict():
     test_out = get_recipe("Arrabiata")
     assert type(test_out) == dict
     assert test_out["strMeal"] == "Spicy Arrabiata Penne"
 
-def test_strip_ingredients():
-    test_out = strip_ingredients('test_json.json')
-    print(test_out)
-    assert False
+def test_strip_ingredients_reads_file():
+    test_out = strip_ingredients('test/test_json.json')
+    assert type(test_out) == dict
+    test_data = {"Hummus, commercial": "test"}
+    assert test_out.keys() == test_data.keys()
